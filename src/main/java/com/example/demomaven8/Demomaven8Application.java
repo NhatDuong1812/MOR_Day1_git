eca67d7bce4f05d6da0d2563e48fcf27ced34c0d
hoangduong181201@gmail.com <Duong181201>
2021-04-22 14:08:47 +0700

    Duong

---
+++ src/main/java/com/example/demomaven8/Demomaven8Application.java
@@ -0,0 +1,13 @@
+package com.example.demomaven8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demomaven8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demomaven8Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demomaven8/Demomaven8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomaven8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demomaven8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
